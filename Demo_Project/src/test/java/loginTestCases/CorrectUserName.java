559ba4b1f76e28d42a46607f8b500bb136ead291
shanthavadhana <98024277+shanthavadhana@users.noreply.github.com>
2022-02-08 14:59:28 +0530

    Add files via upload

---
+++ Demo_Project/src/test/java/loginTestCases/CorrectUserName.java
@@ -0,0 +1,31 @@
+package loginTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CorrectUserName {
+	
+	@Test
+	@Parameters({"username","password"})
+	private void loginWithCorrectUserName(String uName, String pword) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Zapcom\\eclipse-wrkspace\\SuiteExample\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		Thread.sleep(3000);
+		WebElement userName = driver.findElement(By.id("txtUsername"));
+		userName.sendKeys(uName);
+		WebElement password = driver.findElement(By.id("txtPassword"));
+		password.sendKeys(pword);
+		WebElement loginButton = driver.findElement(By.id("btnLogin"));
+		loginButton.click();
+		Thread.sleep(3000);
+		driver.quit();  
+		
+	}
+}
